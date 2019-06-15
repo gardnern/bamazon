f01c5b372573c5c0631d34def7edd56b70bcbcf5
gardnern <48269958+gardnern@users.noreply.github.com>
2019-06-15 02:24:32 -0600

    added changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,90 +1,119 @@
-// Dependencies
-var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table");
+var mysql = require("mysql");
 
-// Connection script
+// Define the MySQL connection parameters
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon_db"
+	host: "localhost",
+	port: 3306,
+
+	user: "root",
+	password: "root",
+	database: "bamazon_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("You are now connected " + connection.threadId);
-   
-    bamazon();      
+// validateInput makes sure that the user is supplying only positive integers for their inputs
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
 
-});                 
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return "Please enter a whole non-zero number.";
+	}
+}
 
-function bamazon() {
-    connection.query('SELECT * FROM products', function (err, res) {
-        if (err) throw err;
+function userprompt() {
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "item_id",
+			message: "Enter Item ID of the product:",
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: "input",
+			name: "quantity",
+			message: "Enter quantity",
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+		
+		var item = input.item_id;
+		var quantity = input.quantity;
 
-        // cli-Table 
-        var table = new Table(
-            {
-                head: ["Product ID", "Product Name", "Department Name", "Price", "Quantity"],
-                colWidths: [20, 30, 19, 12, 12],
-            },
-            { chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'mid-mid': '', 'right-mid': ''} 
-            });
-     
-        for (var i = 0; i < res.length; i++) {
-            table.push(
-                [res[i].item_id, res[i].product_name, res[i].department_name, parseFloat(res[i].price).toFixed(2), res[i].stock_quantity]
-            );
-        }
-        console.log(table.toString());
-        // Prompt users with two messages.
-        // * The first should ask them the ID of the product they would like to buy.
-        // * The second message should ask how many units of the product they would like to buy.
-        inquirer.prompt([
-            {
-                type: "number",
-                message: "Enter the item ID of the product that you would like to buy? ",
-                name: "id"
-            },
-            {
-                type: "number",
-                message: "Enter the quantity you would like to purchase? ",
-                name: "quantity"
-            },
-        ])
+		var queryStr = "SELECT * FROM products WHERE ?";
 
-            // Ordering function
-            .then(function (cart) {
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
 
-                var quantity = cart.quantity;
-                var itemID = cart.id;
+			if (data.length === 0) {
+				console.log("Item ID not valid, try again!");
+				displayInventory();
 
-                connection.query('SELECT * FROM products WHERE id=' + item_id, function (err, selectedItem) {
-                    if (err) throw err;
-                   
-                    if (selectedItem[0].stock_quantity - quantity >= 0) {
+			} else {
+				var productData = data[0];
 
-                        console.log("Quantity in Stock: " + selectedItem[0].stock_quantity + " Order Quantity: " + quantity);
+				if (quantity <= productData.stock_quantity) {
+					console.log("Product in stock. Ready to place the order");
 
-                        console.log("Suffiecient inventory of " + selectedItem[0].product_name + " to fill your order!");
+					
+					var updateQueryStr = "UPDATE products SET stock_quantity = " + (productData.stock_quantity - quantity) + " WHERE item_id = " + item;
+					
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
 
-                        console.log("Total order: $ " + (cart.quantity * selectedItem[0].price).toFixed(2));
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log("\n*******************************************************************\n");
 
-                        connection.query('UPDATE products SET stock_quantity=? WHERE id=?', [selectedItem[0].stock_quantity - quantity, itemID],
+						connection.end();
+					})
+				} else {
+					console.log("That item is out of stock. Try another item.");
+					console.log("\n****************************************************************************\n");
 
-                            function (err, inventory) {
-                                if (err) throw err;
+					displayInventory();
+				}
+			}
+		})
+	})
+}
 
-                                bamazon();  
-                            });  
-                    }
-                    else {
-                        console.log("Insufficient Inventory: " + selectedItem[0].stock_quantity + " " + selectedItem[0].product_name + " in stock. \nPlease make another selection or reduce your quantity.");
-                                bamazon();  
-                    }
-                });
-            });
-    });
-}  
+function displayInventory() {
+
+	queryStr = "SELECT * FROM products";
+
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log("Existing Inventory: ");
+		console.log("-------------------------------------------\n");
+
+		var strOut = "";
+		for (var i = 0; i < data.length; i++) {
+			strOut = "";
+			strOut += "Item ID: " + data[i].item_id + "  //  ";
+			strOut += "Product Name: " + data[i].product_name + "  //  ";
+			strOut += "Department: " + data[i].department_name + " // ";
+			strOut += "Price: $" + data[i].price + "\n";
+			strOut += "Stock: " + data[i].stock_quantity + "\n";
+
+			console.log(strOut);
+		}
+
+	  	console.log("******************************************************\n");
+
+	  
+	  	userprompt();
+	})
+}
+
+function runBamazon() {
+
+	displayInventory();
+}
+
+
+runBamazon();
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -4,7 +4,7 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-  item_id int AUTO_INCREMENT,
+  item_id int AUTO INCREMENT,
   product_name varchar(40) NOT NULL,
   department_name varchar(40) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
@@ -15,4 +15,9 @@ INSERT INTO products (product_name, department_name, price, stock_quantity) VALU
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Presto Pressure Cooker", "kitchen", 45.50, 150);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Sharp Blender", "kitchen", 80.00, 100);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Elekty Scale", "kitchen", 15.95, 80);
-    
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Lodge Dutch Oven", "kitchen", 39.99, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Squid Face Paint", "art and crafts", 9.99, 150);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Canson Water Color", "art and crafts", 5.97, 130);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Melissa & Doug Sketch Pad", "art and crafts", 7.97, 200);    
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Game of Thrones", "books", 41.99, 300);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("A Bird Box: A Novel", "books", 28.99, 500);    
